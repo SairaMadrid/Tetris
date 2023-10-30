5bf9a01937092535567bdfee384a6b0dd18fac7d
Saira <119068271+SairaMadrid@users.noreply.github.com>
2023-10-30 15:11:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const lTetromino = "firstShape";
+const zTetromino = "secondShape";
+const oTetromino = "thirdShape";
+const iTetromino = "fourthShape";
+const tTetromino = "fifthShape";
+
+const tetrominoes = [
+  lTetromino,
+  zTetromino,
+  oTetromino,
+  iTetromino,
+  tTetromino,
+];
+
+console.log(tetrominoes[4]);
+
+/* let names = ["Ania", "Dave", "Hannah", "Bob"];
+
+names.forEach((name) => {
+  console.log(name + " is the best");
+}); */
